394fb64d2715c3778a5ec24a8c12d3097c5d29b4
SirBlobman <blobmansrealm@gmail.com>
2023-07-03 22:56:05 -0400

    Fix bukkit task cancellation failing on legacy servers (1.8.8)

--- bukkit/src/main/java/com/github/sirblobman/api/folia/task/WrappedBukkitTask.java
+++ bukkit/src/main/java/com/github/sirblobman/api/folia/task/WrappedBukkitTask.java
@@ -2,9 +2,27 @@ package com.github.sirblobman.api.folia.task;
 
 import org.jetbrains.annotations.NotNull;
 
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
 
 public final class WrappedBukkitTask extends WrappedTask {
+    private static final boolean LEGACY_CANCEL;
+
+    static {
+        boolean legacy;
+        try {
+            Class<?> class_BukkitTask = Class.forName("org.bukkit.scheduler.BukkitTask");
+            class_BukkitTask.getMethod("isCancelled");
+            class_BukkitTask.getMethod("cancel");
+            legacy = false;
+        } catch (ReflectiveOperationException ex) {
+            legacy = true;
+        }
+
+        LEGACY_CANCEL = legacy;
+    }
+
     private final BukkitTask task;
 
     public WrappedBukkitTask(@NotNull BukkitTask task) {
@@ -18,15 +36,31 @@ public final class WrappedBukkitTask extends WrappedTask {
 
     @Override
     public void cancel() {
-        BukkitTask task = getTask();
-        if (!task.isCancelled()) {
-            task.cancel();
+        if (LEGACY_CANCEL) {
+            cancelLegacy();
+        } else {
+            getTask().cancel();
         }
     }
 
     @Override
     public boolean isCancelled() {
-        BukkitTask task = getTask();
-        return task.isCancelled();
+        if (LEGACY_CANCEL) {
+            return isCancelledLegacy();
+        }
+
+        return getTask().isCancelled();
+    }
+
+    private boolean isCancelledLegacy() {
+        int taskId = getTask().getTaskId();
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        return (!scheduler.isQueued(taskId) && !scheduler.isCurrentlyRunning(taskId));
+    }
+
+    private void cancelLegacy() {
+        int taskId = getTask().getTaskId();
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        scheduler.cancelTask(taskId);
     }
 }
